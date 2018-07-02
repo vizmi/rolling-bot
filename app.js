98ef5119db31659ea8d3eea855bca568f88656f8
Mihaly Vizhanyo <vizhanyo.mihaly@gmail.com>
2018-07-02 19:45:19 +0200

    actual rolling function

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
 
+var types = require("./dice-definitions");
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
@@ -27,5 +29,51 @@ bot.set('storage', inMemoryStorage);
 
 // Respond
 bot.dialog('/', function (session) {
-    session.send('You: ' + session.message.text);
+    var msg = session.message.text.toLocaleLowerCase().split(' ');
+    if (msg[0] === 'roll') {
+        msg.shift(); // drop the command
+        var total = { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 };
+        msg.forEach((dice) => {
+            console.log(dice);
+
+            // count of dice
+            var count = parseInt(dice);
+            count = Math.max(count, 1);
+            console.log(count);
+
+            // type of dice
+            var type = dice.substring(count.toString().length);
+            var typeFound = types.find((t) => {
+                return (t.names.includes(type));
+            }, this);
+            console.log(typeFound.names[0]);
+
+            for (i = 0; i < count; i++) {
+                var roll = typeFound.faces[Math.trunc(Math.random() * typeFound.faces.length)];
+                console.log(roll);
+                total.advantage += roll.advantage;
+                total.success += roll.success;
+                total.triumph += roll.triumph;
+                total.threat += roll.threat;
+                total.failure += roll.failure;
+                total.despair += roll.despair;
+            }
+        });
+        // summing it up
+        var reply = '';
+        if (total.triumph > 0)
+            reply += 'Triumphs: ' + total.triumph.toString() + '\n';
+        if (total.despair > 0)
+            reply += 'Despairs: ' + total.despair.toString() + '\n';
+        if (total.success > total.failure)
+            reply += 'Successes: ' + (total.success - total.failure).toString() + '\n';
+        if (total.success < total.failure)
+            reply += 'Failures: ' + (total.failure - total.success).toString() + '\n';
+        if (total.advantage > total.threat)
+            reply += 'Advantages: ' + (total.advantage - total.threat).toString() + '\n';
+        if (total.advantage < total.threat)
+            reply += 'Threats: ' + (total.threat - total.advantage).toString() + '\n';
+        if (reply !== '')
+            session.send(reply);
+    }
 });
---
+++ dice-definitions.js
@@ -0,0 +1,84 @@
+module.exports = [
+    {
+        names: ['boost', 'b'],
+        faces: [
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 2, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+        ]
+    },
+    {
+        names: ['setback', 's'],
+        faces: [
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 1, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 1, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 0, despair: 0 },
+        ]
+    },
+    {
+        names: ['ability', 'a'],
+        faces: [
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 2, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 2, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+        ]
+    },
+    {
+        names: ['difficulty', 'd'],
+        faces: [
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 1, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 2, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 2, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 1, despair: 0 },
+        ]
+    },
+    {
+        names: ['proficiency', 'p'],
+        faces: [
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 2, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 2, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 1, success: 1, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 2, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 2, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 1, threat: 0, failure: 0, despair: 0 },
+        ]
+    },
+    {
+        names: ['challenge', 'c'],
+        faces: [
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 1, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 1, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 2, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 2, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 1, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 1, failure: 1, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 2, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 2, failure: 0, despair: 0 },
+            { advantage: 0, success: 0, triumph: 0, threat: 0, failure: 0, despair: 1 },
+        ]
+    },
+]
